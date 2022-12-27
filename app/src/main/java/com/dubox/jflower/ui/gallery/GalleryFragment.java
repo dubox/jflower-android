046694f271ff3373dc0b8c3538af44f48c50a115
dubox <dubo_x@qq.com>
2022-12-27 23:12:06 +0800

    about page

--- app/src/main/java/com/dubox/jflower/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/dubox/jflower/ui/gallery/GalleryFragment.java
@@ -1,5 +1,7 @@
 package com.dubox.jflower.ui.gallery;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,22 +12,33 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.dubox.jflower.MainActivity;
 import com.dubox.jflower.databinding.FragmentGalleryBinding;
+
+import static com.google.android.material.internal.ContextUtils.getActivity;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
+    private MainActivity mainActivity ;
+    protected GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
+        galleryViewModel =
                 new ViewModelProvider(this).get(GalleryViewModel.class);
+        mainActivity = (MainActivity)getActivity();
 
         binding = FragmentGalleryBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         final TextView textView = binding.textGallery;
+        final TextView newV = binding.newV;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        galleryViewModel.getNewV().observe(getViewLifecycleOwner(), newV::setText);
+
+        this.setV();
+
         return root;
     }
 
@@ -33,5 +46,17 @@ public class GalleryFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    protected void setV(){
+        PackageManager manager = mainActivity.getPackageManager();
+        PackageInfo info = null;
+        try {
+            info = manager.getPackageInfo(mainActivity.getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String version = info.versionName;
+        galleryViewModel.setV(version);
     }
 }
--- app/src/main/java/com/dubox/jflower/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/dubox/jflower/ui/gallery/GalleryViewModel.java
@@ -1,19 +1,40 @@
 package com.dubox.jflower.ui.gallery;
 
+import android.content.pm.PackageManager;
+
+import com.dubox.jflower.MainActivity;
+import com.dubox.jflower.R;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import static com.google.android.material.internal.ContextUtils.getActivity;
+
 public class GalleryViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
+    private final MutableLiveData<String> newVText;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        newVText = new MutableLiveData<>();
+
+        mText.setValue("Current version: " );
+        newVText.setValue("New version: " );
+    }
+
+    public void setV(String v){
+        mText.setValue("Current version: "+ v);
+    }
+    public void setNewV(String v){
+        newVText.setValue("New version: "+ v);
     }
 
     public LiveData<String> getText() {
         return mText;
+    }
+    public LiveData<String> getNewV() {
+        return newVText;
     }
 }
