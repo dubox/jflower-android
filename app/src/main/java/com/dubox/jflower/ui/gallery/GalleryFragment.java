87f33a1d6e205e421f56c72e503c99f4439f0614
dubox <dubo_x@qq.com>
2022-12-29 00:46:20 +0800

    about

--- app/src/main/java/com/dubox/jflower/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/dubox/jflower/ui/gallery/GalleryFragment.java
@@ -2,11 +2,18 @@ package com.dubox.jflower.ui.gallery;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,14 +21,19 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.dubox.jflower.MainActivity;
 import com.dubox.jflower.databinding.FragmentGalleryBinding;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.AsyncHttpRequest;
+import com.koushikdutta.async.http.AsyncHttpResponse;
 
-import static com.google.android.material.internal.ContextUtils.getActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class GalleryFragment extends Fragment {
 
     private FragmentGalleryBinding binding;
     private MainActivity mainActivity ;
     protected GalleryViewModel galleryViewModel;
+    protected PackageInfo pInfo;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,10 +46,13 @@ public class GalleryFragment extends Fragment {
 
         final TextView textView = binding.textGallery;
         final TextView newV = binding.newV;
+        final Button upBtn = binding.button;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         galleryViewModel.getNewV().observe(getViewLifecycleOwner(), newV::setText);
+        galleryViewModel.getBtnShow().observe(getViewLifecycleOwner(), upBtn::setVisibility);
 
         this.setV();
+        this.getNewV();
 
         return root;
     }
@@ -50,13 +65,53 @@ public class GalleryFragment extends Fragment {
 
     protected void setV(){
         PackageManager manager = mainActivity.getPackageManager();
-        PackageInfo info = null;
+
         try {
-            info = manager.getPackageInfo(mainActivity.getPackageName(), 0);
+            pInfo = manager.getPackageInfo(mainActivity.getPackageName(), 0);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        String version = info.versionName;
+        String version = pInfo.versionName;
         galleryViewModel.setV(version);
+    }
+
+    protected void getNewV(){
+        //
+//        System.out.println(View.);
+        AsyncHttpClient.getDefaultInstance().executeJSONObject(new AsyncHttpRequest(
+                Uri.parse(
+//                        "https://gitee.com/dubox/jflower-android/raw/master/version.json"
+//                        "https://raw.githubusercontent.com/dubox/jflower-android/master/version.json"
+                        "https://dubox.github.io/jflower-android/version.json"
+                ), "GET"), new AsyncHttpClient.JSONObjectCallback() {
+
+            // Callback is invoked with any exceptions/errors, and the result, if available.
+            @Override
+            public void onCompleted(Exception e, AsyncHttpResponse response, JSONObject result) {
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        Looper.prepare();
+                        if (e != null) {
+                            System.out.println(e.toString());
+                            //e.printStackTrace();
+                            Toast.makeText(getContext(),"网络异常，请稍后重试",Toast.LENGTH_LONG).show();
+                            return;
+                        }
+                        System.out.println("I got a JSONObject: " + result);
+                        try {
+                            galleryViewModel.setNewV(result.get("name").toString());
+                            galleryViewModel.setBtnShow((int)result.get("code") - pInfo.versionCode);
+                        } catch (JSONException ex) {
+                            ex.printStackTrace();
+                        }
+                        Looper.loop();
+                    }
+                }).start();
+
+            }
+        });
     }
 }
--- app/src/main/java/com/dubox/jflower/ui/gallery/GalleryViewModel.java
+++ app/src/main/java/com/dubox/jflower/ui/gallery/GalleryViewModel.java
@@ -1,9 +1,16 @@
 package com.dubox.jflower.ui.gallery;
 
 import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.view.View;
 
 import com.dubox.jflower.MainActivity;
 import com.dubox.jflower.R;
+import com.koushikdutta.async.http.AsyncHttpClient;
+import com.koushikdutta.async.http.AsyncHttpRequest;
+import com.koushikdutta.async.http.AsyncHttpResponse;
+
+import org.json.JSONObject;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -15,22 +22,31 @@ public class GalleryViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
     private final MutableLiveData<String> newVText;
+    private final MutableLiveData<Integer> showUpBtn;
 
     public GalleryViewModel() {
         mText = new MutableLiveData<>();
         newVText = new MutableLiveData<>();
+        showUpBtn = new MutableLiveData<>();
 
         mText.setValue("Current version: " );
         newVText.setValue("New version: " );
+//        showUpBtn.setValue(0);
     }
 
     public void setV(String v){
         mText.setValue("Current version: "+ v);
     }
     public void setNewV(String v){
-        newVText.setValue("New version: "+ v);
+        newVText.postValue("New version: "+ v);
+    }
+    public void setBtnShow(int show){
+        showUpBtn.postValue( show==0? View.INVISIBLE:View.VISIBLE);
     }
 
+    public LiveData<Integer> getBtnShow() {
+        return showUpBtn;
+    }
     public LiveData<String> getText() {
         return mText;
     }
